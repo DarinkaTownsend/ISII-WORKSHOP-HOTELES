0103ffb84bf591c1d2e0871312def52e480ed572
keanceva <keanceva@espol.edu.ec>
2021-10-18 21:22:12 -0500

    Setter

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -7,5 +7,17 @@ public class CarInsurance {
 	private static final double BasePremium = 500;
 	private boolean isValidLicense;
 	// To DO make logical process of car Insurance
+	public void setCustomerAge(int customerAge) {
+		CustomerAge = customerAge;
+	}
+	public void setCustomerGender(char customerGender) {
+		CustomerGender = customerGender;
+	}
+	public void setCivilStatus(String civilStatus) {
+		CivilStatus = civilStatus;
+	}
+	public void setValidLicense(boolean isValidLicense) {
+		this.isValidLicense = isValidLicense;
+	}
 	
 }
