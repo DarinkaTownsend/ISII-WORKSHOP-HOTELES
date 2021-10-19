1e3d0e4f3aab386d1880f250f4838d1c487052b0
Jaime Alarcón <jaiaalar@espol.edu.ec>
2021-10-18 21:18:58 -0500

    Attributes

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,5 +1,11 @@
 package ec.edu.espol.workshops;
-
+ 
 public class CarInsurance {
-
+	private int CustomerAge;
+	private char CustomerGender;
+	private String CivilStatus;
+	private static final double BasePremium = 500;
+	private boolean isValidLicense;
+	// To DO make logical process of car Insurance
+	
 }
