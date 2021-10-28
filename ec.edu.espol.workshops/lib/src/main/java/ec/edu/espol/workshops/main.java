4c407711bc88cf6cfefb3385d43bb3e87875c5d5
DarinkaTownsend <darinka.townsend@hotmail.com>
2021-10-28 12:27:41 -0500

    Code for take the time execute

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/main.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/main.java
@@ -7,8 +7,10 @@ public class  main {
 
 	public static void main(String[] args) {
 		
-	System.out.println ("¡WELCOME TO YOU CAR-INSURANCE!");
-	System.out.println ("------------------------------------------");
+		long TInicio, TFin, tiempo; //Variables para determinar el tiempo de ejecución
+		TInicio = System.currentTimeMillis(); 
+		System.out.println ("¡WELCOME TO YOU CAR-INSURANCE!");
+		System.out.println ("------------------------------------------");
 		
 		
 		
@@ -106,7 +108,9 @@ public class  main {
 		
 		
 		
-		
+		TFin = System.currentTimeMillis(); //Tomamos la hora en que finalizó el algoritmo y la almacenamos en la variable T
+		tiempo = TFin - TInicio; //Calculamos los milisegundos de diferencia
+		System.out.println("Tiempo de ejecución en milisegundos: " + tiempo); //Mostramos en pantalla el tiempo de ejecución en milisegundos
 		
 
 	}
